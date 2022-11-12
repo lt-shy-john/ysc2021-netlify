1b55cc2bb372a8056fce5e1847c25c01f550e90f
John Yeung <37952085+lt-shy-john@users.noreply.github.com>
2022-11-12 17:35:53 +1100

    Delete the site for favouring the static site

--- wp-content/uploads/sucuri/sucuri-ignorescanning.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=ignorescanning;
-// created_on=1627560145;
-// updated_on=1627560145;
-exit(0);
-?>
--- wp-content/uploads/sucuri/sucuri-integrity.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=integrity;
-// created_on=1627560146;
-// updated_on=1627560146;
-exit(0);
-?>
